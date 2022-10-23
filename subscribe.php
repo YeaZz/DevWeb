9784390e09fa2d66b06c0cff5c304b9bc619c36a
Noa <perhapsitisyeazz@gmail.com>
2022-10-23 15:21:50 +0200

    oublie link css

--- subscribe.php
+++ subscribe.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
 </head>
 <body>
 
