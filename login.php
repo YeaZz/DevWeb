b4658719c3794d2643dfbe3a0ff40402f8e3caa0
Noa <perhapsitisyeazz@gmail.com>
2022-11-07 17:00:53 +0100

    Connexion admin

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include("modules/head.php") ?>
+<body>
+
+    <?php include("modules/header.php"); ?>
+
+    <main style="margin: 9em 0;">
+        <h1>Formulaire de connexion</h1>  
+        <form action="treatment/login.php" method="POST">
+            <div>
+                <label for="id">Identifiant</label>
+                <input id="id" name="id" type="text" required autofocus>
+            </div>
+            <div>
+                <label for="password">Mot de passe</label>
+                <input id="password" name="password" type="password" required>
+            </div>
+            <label><input type="checkbox" value="remember-me"> Se souvenir de moi</label>
+            <button type="submit">
+                <p>Connexion</p>
+                <i class="icon-arrow-right"></i>
+            </button>
+        </form>
+    </main>
+
+    <?php include("modules/footer.php"); ?>
+
+</body>
+</html>
