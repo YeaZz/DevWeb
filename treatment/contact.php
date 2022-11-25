2bc14f98aabdba2a68d15bc023b18469b89f3fe6
Noa <perhapsitisyeazz@gmail.com>
2022-11-25 15:29:43 +0100

    saving message

--- treatment/contact.php
+++ treatment/contact.php
@@ -2,18 +2,20 @@
 
 include("../include/config.php");
 
+session_start();
+
 if (!empty($_POST) && isset($_SESSION["id"]) && isset($_POST["subject"]) && isset($_POST["message"])) {
-    $subject = $_POST["subject"];
     $id = $_SESSION["id"];
+    $subject = $_POST["subject"];
     $message = $_POST["message"];
     if ($pdo != null) {
-        $query = $pdo->prepare("INSERT INTO p2107540.messages VALUES (0, :subject, :user, :message)");
+        $query = $pdo->prepare("INSERT INTO messages VALUES (0, :subject, :user, :message)");
         $query->execute([
             'subject' => $subject,
             'user' => $id,
             'message' => $message
         ]);
-        header("../index.php");
+        header("Location: ../index.php");
         return;
     }
 }
